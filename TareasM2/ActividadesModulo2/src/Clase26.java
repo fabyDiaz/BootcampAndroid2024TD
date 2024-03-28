677c5520e1b56166375f83715f5cc3a5df631ece
fabyDiaz <fbl.diaz.n@gmail.com>
2024-03-28 20:56:10 -0300

    Actualizamos clase 26

--- TareasM2/ActividadesModulo2/src/Clase26.java
+++ TareasM2/ActividadesModulo2/src/Clase26.java
@@ -1,2 +1,13 @@
 public class Clase26 {
+    public static void main(String[] args) {
+        int x = 0;
+        depurar(x);
+    }
+
+    public static void depurar(int x){
+        while (x<1){
+            System.out.println("Ciclo");
+            x=x+1; //Faltaba actualizar x, de lo contrario el bucle es infinito
+        }
+    }
 }
